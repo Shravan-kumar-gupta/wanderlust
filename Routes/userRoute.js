16e30a7f468104d9ce70bbeeddc512d2dd0a4168
Shravan-kumar-gupta <kumarshravan1268@gmail.com>
2025-03-08 23:25:25 +0530

    Added Project files

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const { saveRedirectUrl } = require("../middleware");
-const User = require("../models/User");
+const User = require('../models/User');
 const passport = require("passport")
 const userController = require("../controller/userController");
 
