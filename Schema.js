322438cafc994043ea820a60b65d187d9b95adb8
Shravan-kumar-gupta <kumarshravan1268@gmail.com>
2025-05-01 22:37:44 +0530

    added files with git

--- Schema.js
+++ Schema.js
@@ -1,5 +1,4 @@
-const joi = require('joi')
-const {forbidden} = require("joi");
+const joi = require('joi');
 
 module.exports.listingSchema = joi.object({
     listing: joi.object({
@@ -9,28 +8,18 @@ module.exports.listingSchema = joi.object({
         country: joi.string().required(),
         price: joi.number().required().min(0),
 
-        // Allow images to be optional for updates
-        image: joi.alternatives().try(
-            joi.array().items(
-                joi.object({
-                    url: joi.string().uri().required(),
-                    filename: joi.string().required()
-                })
-                .min(0)
-                .allow(null)
-            ),
-        )
+        image: joi.array().items(
+            joi.object({
+                url: joi.string().uri().required(),
+                filename: joi.string().required()
+            })
+        ).optional().allow(null)
     }).required()
 });
-module.exports.reviewSchema  =  joi.object({
-    review : joi.object({
-        rating : joi.number().required().min(1).max(5),
-        comment : joi.string().required(),
 
+module.exports.reviewSchema = joi.object({
+    review: joi.object({
+        rating: joi.number().required().min(1).max(5),
+        comment: joi.string().required(),
     }).required()
-})
-<<<<<<< HEAD
-=======
-// image : joi.string().allow("",null),
->>>>>>> 21bac896e0361c20a24e6c3ebee173b60ea5ae16
-
+});
